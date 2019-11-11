589c3b68ddbc6fe879dcd7880e394a97ac77c7bc
Ana Boca <Ana.Boca@asu.edu>
2019-11-11 21:18:54 +0000

    Lesson 16 partial commit

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,10 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
+  def index
+    @grams = Gram.all
+  end
+
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
@@ -54,7 +58,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
